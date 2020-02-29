479cec1bc08f1131c9419d29c8169ecd18de96d2
Mani Jafari <manielli@gmail.com>
2020-02-28 17:25:50 -0800

    Change Stylesheet to material-ui/styles using makeStyles

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles'
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
+const useStyles = makeStyles({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-});
+  }
+})
+
+export default function App() {
+  const classes = useStyles()
+
+  return (
+    <View className={classes.container} >
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
