4cce8a4ebf374e8481a8784a527265a28496bb1f
Mani Jafari <manielli@gmail.com>
2020-04-03 21:25:42 -0700

    Add navigation logic to WelcomePage, to navigate to Sign In/Up Page if currentUser is not present

--- components/WelcomePage.js
+++ components/WelcomePage.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import { View } from 'react-native'
 import { Spinner } from './common'
+import { useSelector } from 'react-redux'
 
-const WelcomePage = () => {
+const WelcomePage = ({ navigation }) => {
+    const authenticatedUser = useSelector(state => state.auth)
+    console.log(authenticatedUser)
+    
+    const renderSpinner = () => {
+        if (authenticatedUser) {
+            navigation.navigate('Sign In/Up')
+        } 
+        return (
+            <Spinner />
+        )
+    }
+    
     return (
-        <Spinner />
+        <View style={{backgroundColor: 'steelblue'}} >
+            {renderSpinner()}
+        </View>
     )
 }
 
