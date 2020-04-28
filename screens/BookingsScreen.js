6dd8cf106d757c2d0ae88bea8ff22426276579bb
Mani Jafari <manielli@gmail.com>
2020-04-28 10:26:21 -0700

    Add useSelector hook to BookingsScreen component to access auth piece of application level state

--- screens/BookingsScreen.js
+++ screens/BookingsScreen.js
@@ -2,8 +2,12 @@ import React from 'react'
 import { Card, CardSection } from '../components/common'
 import { Text } from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient'
+import { useSelector, useDispatch } from 'react-redux'
 
 const BookingsScreen = ({navigation}) => {
+    const authState = useSelector(state => state.auth)
+    console.log(authState)
+    
     return (
         <LinearGradient
             style={styles.container}
